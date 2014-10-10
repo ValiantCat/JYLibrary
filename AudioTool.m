f4424e3bff852e31a212d4eefbc8316cd72ecbfc
aiqiuqiu <519224747@qq.com>
2014-10-10 12:06:32 +0800

    audioTool
    增加了播放音乐 暂停播放和停止播放

--- AudioTool.m
+++ AudioTool.m
@@ -7,17 +7,40 @@
 //  音效工具类
 
 #import "AudioTool.h"
-#import <AVFoundation/AVFoundation.h>
+
 @implementation AudioTool
 
-// 字典: filename作为key, soundID作为value
-// 存放所有的音频ID
+/**
+ *  存放所有的音频ID
+ *  字典: filename作为key, soundID作为value
+ */
 static NSMutableDictionary *_soundIDDict;
+
+/**
+ *  存放所有的音乐播放器
+ *  字典: filename作为key, audioPlayer作为value
+ */
+static NSMutableDictionary *_audioPlayerDict;
+
+/**
+ *  初始化
+ */
 + (void)initialize
 {
     _soundIDDict = [NSMutableDictionary dictionary];
+    _audioPlayerDict = [NSMutableDictionary dictionary];
+    
+    // 设置音频会话类型
+    AVAudioSession *session = [AVAudioSession sharedInstance];
+    [session setCategory:AVAudioSessionCategorySoloAmbient error:nil];
+    [session setActive:YES error:nil];
 }
 
+/**
+ *  播放音效
+ *
+ *  @param filename 音效文件名
+ */
 + (void)playSound:(NSString *)filename
 {
     if (!filename) return;
@@ -31,7 +54,7 @@ static NSMutableDictionary *_soundIDDict;
         if (!url) return;
         
         // 创建音效ID
-        AudioServicesCreateSystemSoundID((__bridge CFURLRef)(url), &soundID);
+        AudioServicesCreateSystemSoundID((__bridge CFURLRef)url, &soundID);
         
         // 放入字典
         _soundIDDict[filename] = @(soundID);
@@ -41,6 +64,11 @@ static NSMutableDictionary *_soundIDDict;
     AudioServicesPlaySystemSound(soundID);
 }
 
+/**
+ *  销毁音效
+ *
+ *  @param filename 音效文件名
+ */
 + (void)disposeSound:(NSString *)filename
 {
     if (!filename) return;
@@ -55,5 +83,96 @@ static NSMutableDictionary *_soundIDDict;
     }
 }
 
+/**
+ *  播放音乐
+ *
+ *  @param filename 音乐文件名
+ */
++ (AVAudioPlayer *)playMusic:(NSString *)filename
+{
+    if (!filename) return nil;
+    
+    // 1.从字典中取出audioPlayer
+    AVAudioPlayer *audioPlayer = _audioPlayerDict[filename];
+    if (!audioPlayer) { // 创建
+        // 加载音乐文件
+        NSURL *url = [[NSBundle mainBundle] URLForResource:filename withExtension:nil];
+        
+        if (!url) return nil;
+        
+        // 创建audioPlayer
+        audioPlayer = [[AVAudioPlayer alloc] initWithContentsOfURL:url error:nil];
+        
+        // 缓冲
+        [audioPlayer prepareToPlay];
+        
+        //        audioPlayer.enableRate = YES;
+        //        audioPlayer.rate = 10.0;
+        
+        // 放入字典
+        _audioPlayerDict[filename] = audioPlayer;
+    }
+    
+    // 2.播放
+    if (!audioPlayer.isPlaying) {
+        [audioPlayer play];
+    }
+    
+    return audioPlayer;
+}
 
+/**
+ *  暂停音乐
+ *
+ *  @param filename 音乐文件名
+ */
++ (void)pauseMusic:(NSString *)filename
+{
+    if (!filename) return;
+    
+    // 1.从字典中取出audioPlayer
+    AVAudioPlayer *audioPlayer = _audioPlayerDict[filename];
+    
+    // 2.暂停
+    if (audioPlayer.isPlaying) {
+        [audioPlayer pause];
+    }
+}
+
+/**
+ *  停止音乐
+ *
+ *  @param filename 音乐文件名
+ */
++ (void)stopMusic:(NSString *)filename
+{
+    if (!filename) return;
+    
+    // 1.从字典中取出audioPlayer
+    AVAudioPlayer *audioPlayer = _audioPlayerDict[filename];
+    
+    // 2.暂停
+    if (audioPlayer.isPlaying) {
+        [audioPlayer stop];
+        
+        // 直接销毁
+        [_audioPlayerDict removeObjectForKey:filename];
+    }
+}
+
+/**
+ *  返回当前正在播放的音乐播放器
+ */
++ (AVAudioPlayer *)currentPlayingAudioPlayer
+{
+    for (NSString *filename in _audioPlayerDict) {
+        AVAudioPlayer *audioPlayer = _audioPlayerDict[filename];
+        
+        if (audioPlayer.isPlaying) {
+            return audioPlayer;
+        }
+    }
+    
+    return nil;
+}
 @end
