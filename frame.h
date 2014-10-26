0632c1c41a27c4cf677c79e5d6018e0172a55e65
aiqiuqiu <519224747@qq.com>
2014-10-26 20:56:01 +0800

    快捷方式设置view  frame

---
+++ frame.h
@@ -0,0 +1,18 @@
+//
+//  frame.h
+//  JYLibrary
+//
+//  Created by nero on 14-10-26.
+//  Copyright (c) 2014年 姜沂. All rights reserved.
+//  快捷方式设置view的frame
+
+#import <UIKit/UIKit.h>
+
+@interface frame : UIView
+@property (assign, nonatomic) CGFloat x;
+@property (assign, nonatomic) CGFloat y;
+@property (assign, nonatomic) CGFloat width;
+@property (assign, nonatomic) CGFloat height;
+@property (assign, nonatomic) CGSize size;
+@property (assign, nonatomic) CGPoint origin;
+@end
